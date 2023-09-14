f9f14104423486cdafdad3b9039153eff2dd1784
Nattalia9 <nattalia.9g@gmail.com>
2023-09-14 13:42:25 -0300

    Entrega mytinerary

--- src/router/itinerariesRouter.js
+++ src/router/itinerariesRouter.js
@@ -2,15 +2,16 @@ const express = require('express')
 const itinerariesRouter = express.Router()
 const { getItineraries, getItinerary, getItinerariesByCity, addItinerary, deleteItinerary, updateItinerary } = require('../controllers/itinerariesController')
 const { verifyDataCity } = require('../middlewares/verifications')
+const { passportVerificator } = require('../middlewares/auth')
 
 const router = express.Router()
 
 itinerariesRouter.get("/", getItineraries)
 itinerariesRouter.get("/:id", getItinerary)
 itinerariesRouter.get("/city/:cityId", getItinerariesByCity)
-itinerariesRouter.post("/", addItinerary)
-itinerariesRouter.delete("/", deleteItinerary)
-itinerariesRouter.put("/:id", updateItinerary)
+itinerariesRouter.post("/", passportVerificator.authenticate("jwt", {session: false} ),  addItinerary)
+itinerariesRouter.delete("/", passportVerificator.authenticate("jwt", {session: false} ), deleteItinerary)
+itinerariesRouter.put("/:id", passportVerificator.authenticate("jwt", {session: false} ), updateItinerary)
 
 
 module.exports = itinerariesRouter
